c27fe7c9b08beb16750d415aed0fb3d649e7b303
Marco <marcooliveirapias@gmail.com>
2020-04-20 21:55:21 +0100

    Blank test case for jenkins build

--- Test Suites/PeeBu.ts
+++ Test Suites/PeeBu.ts
@@ -10,4 +10,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>27100805-04ab-4988-a4ec-44fc155e0b6f</testSuiteGuid>
+   <testCaseLink>
+      <guid>6bc11843-017c-4b3f-8a97-dea0facc5efd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
