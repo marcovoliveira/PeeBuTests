d5e1d137ca8f8005aff796687e85b659d7558b84
Marco <marcooliveirapias@gmail.com>
2020-04-20 21:29:05 +0100

    Add test suite

---
+++ Test Suites/PeeBu.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PeeBu</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>27100805-04ab-4988-a4ec-44fc155e0b6f</testSuiteGuid>
+</TestSuiteEntity>
