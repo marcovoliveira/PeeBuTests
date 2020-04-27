e7055812e8401c838da9de043b69f019badcfa1a
Marco <marcooliveirapias@gmail.com>
2020-04-27 21:48:15 +0100

    Update TS

--- Test Suites/PeeBu.ts
+++ Test Suites/PeeBu.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>FeaturesTestSuite</name>
+   <name>PeeBu</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
